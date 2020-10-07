02b4b5e4a2f47ccd36cc0805f7b89003f62326c7
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2020-10-07 13:50:08 +0900

    users tableの変更

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,25 +8,32 @@
     <div class="card mt-3">
       <div class="card-body">
         <div class="d-flex flex-row">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            <i class="fas fa-user-circle fa-3x"></i>
-          </a>
+          <div class="ml-lg-4">
+            <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
+            	<i class="fas fa-user-circle fa-3x"></i>
+            </a>
+          </div>
+          
+          <div class="mt-2 ml-lg-5 ml-md-4 ml-sm-2">
+            <a href="" class="text-muted pr-lg-3">
+              12 登録
+            </a>
+            <a href="" class="text-muted pr-lg-3">
+              10 フォロー
+            </a>
+            <a href="" class="text-muted pr-lg-3">
+              10 フォロワー
+            </a>
         </div>
-        <h2 class="h5 card-title m-0">
+        </div>
+        <h2 class="h5 card-title mt-1 ml-lg-4">
           <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             {{ $user->name }}
           </a>
         </h2>
-      </div>
+			</div>
       <div class="card-body">
-        <div class="card-text">
-          <a href="" class="text-muted">
-            10 フォロー
-          </a>
-          <a href="" class="text-muted">
-            10 フォロワー
-          </a>
-        </div>
+        <div class="card-text">ここに自己紹介文</div>
       </div>
     </div>
   </div>
